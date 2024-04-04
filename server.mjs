d4d3ba397d6f0e256cb7c038e40c04955c8acc98
hegelminniti <hegelminniti@gmail.com>
2024-04-04 12:37:27 +0200

    Esercizio 6

--- server.mjs
+++ server.mjs
@@ -5,11 +5,36 @@ const server = createServer((request, response) => {
 
     response.statusCode = 200;
 
-    response.setHeader("Content-Type", "text/html")
+    response.setHeader("Content-Type", "application/json")
 
-    response.end("<html><body><h1>Questo è il server di Hegel!</h1></body></html>")
+    const jsonResponseBody = JSON.stringify({ location: "Mars" })
+
+    response.end(jsonResponseBody)
 });
 
 server.listen(3000, () => {
     console.log('Server is running at http://localhost:3000')
 });
+
+
+/* StatusCode        : 200
+StatusDescription : OK
+Content           : {"location":"Mars"}
+RawContent        : HTTP/1.1 200 OK
+                    Connection: keep-alive
+                    Keep-Alive: timeout=5
+                    Content-Length: 19 -----è il valore del corpo della risposta (in bytes). Quindi la risposta in questo caso contiene 19 bytes di dati.
+                    Content-Type: application/json
+                    Date: Thu, 04 Apr 2024 10:31:43 GMT
+
+                    {"location":"Mars"}
+Forms             : {}
+Headers           : {[Connection, keep-alive], [Keep-Alive, timeout=5],       
+                    [Content-Length, 19], [Content-Type,
+                    application/json]...}
+Images            : {}
+InputFields       : {}
+Links             : {}
+ParsedHtml        : System.__ComObject
+RawContentLength  : 19
+ */
